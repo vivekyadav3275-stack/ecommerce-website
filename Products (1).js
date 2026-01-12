ff227834c564d73c7b9a290eb8a64e35d4e7a9a9
vivekyadav3275-stack <vivekyadav3275@gmail.com>
2026-01-12 09:06:44 +0530

    Add files via upload
    product

---
+++ Products (1).js
@@ -0,0 +1,24 @@
+// import image from ProductDetails
+import productpic from "../ProductDetails/productpic.JPEG";
+import laptop  from  "../ProductDetails/laptop.JPEG";
+
+const products = [
+  {
+    id: 1,
+    name: "i phone",
+    price: 90000,
+    image: productpic,   
+    stock: 10
+  },
+  {
+    id: 2,
+    name: "laptop",
+    price: 69999,
+    image: laptop,   
+    stock: 10
+  },
+
+
+];
+
+export default products;
